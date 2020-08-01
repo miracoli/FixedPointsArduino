509c0043fa3dad96a1d8b860b1a732f38a1fdfab
Dirk Petrautzki <dirk@dirk-petrautzki.de>
2020-08-01 14:13:02 +0200

    Add more tests

--- tst/FixedPointsTest.cpp
+++ tst/FixedPointsTest.cpp
@@ -16,7 +16,7 @@ using namespace std;
 
 BOOST_AUTO_TEST_SUITE(testsuite)
 
-BOOST_AUTO_TEST_CASE(test_getters) {
+BOOST_AUTO_TEST_CASE(test_getters_ufixed) {
   UQ8x8 a = 1.5;
   BOOST_CHECK( sizeof(uint16_t) == sizeof(a.getInternal()));
   BOOST_CHECK( 0x0180 == a.getInternal());
@@ -28,7 +28,7 @@ BOOST_AUTO_TEST_CASE(test_getters) {
   BOOST_CHECK( 0x80 == a.getFraction());
 }
 
-BOOST_AUTO_TEST_CASE(test_cast_operators) {
+BOOST_AUTO_TEST_CASE(test_cast_operators_ufixed) {
   UQ8x8 a = 1.5;
   UQ16x16 b = 1.5;
   BOOST_CHECK( 1.5 == static_cast<float>(a) );
@@ -37,5 +37,53 @@ BOOST_AUTO_TEST_CASE(test_cast_operators) {
   //BOOST_CHECK( 1 == static_cast<unsigned int>(a) );
   BOOST_CHECK( b == static_cast<UQ16x16>(a) );
 }
+
+BOOST_AUTO_TEST_CASE(test_getters_sfixed) {
+  SQ7x8 a = 1.5;
+  BOOST_CHECK( sizeof(uint16_t) == sizeof(a.getInternal()));
+  BOOST_CHECK( 0x0180 == a.getInternal());
+
+  BOOST_CHECK( sizeof(uint8_t) == sizeof(a.getInteger()));
+  BOOST_CHECK( 0x01 == a.getInteger());
+
+  BOOST_CHECK( sizeof(uint8_t) == sizeof(a.getFraction()));
+  BOOST_CHECK( 0x80 == a.getFraction());
+}
+
+BOOST_AUTO_TEST_CASE(test_cast_operators_sfixed) {
+  SQ7x8 a = 1.5;
+  SQ15x16 b = 1.5;
+  BOOST_CHECK( 1.5 == static_cast<float>(a) );
+  BOOST_CHECK( 1.5 == static_cast<double>(a) );
+  BOOST_CHECK( 1.5 == static_cast<long double>(a) );
+  //BOOST_CHECK( 1 == static_cast<unsigned int>(a) );
+  BOOST_CHECK( b == static_cast<SQ15x16>(a) );
+}
+
+BOOST_AUTO_TEST_CASE(test_utils) {
+  UQ8x8 a = 1.5;
+  UQ8x8 b = floorFixed(a);
+  BOOST_CHECK( 1.0 == static_cast<float>(b));
+}
+
+BOOST_AUTO_TEST_CASE(test_random) {
+  auto a = randomUFixed<8, 8>();
+  BOOST_CHECK( sizeof(uint16_t) == sizeof(a.getInternal()));
+  BOOST_CHECK( sizeof(uint8_t) == sizeof(a.getInteger()));
+  BOOST_CHECK( sizeof(uint8_t) == sizeof(a.getFraction()));
+}
+
+BOOST_AUTO_TEST_CASE(test_multiply_ufixed) {
+  UQ8x8 a = 200.0;
+  UQ8x8 b = 1.5;
+  BOOST_CHECK( 300.0 == static_cast<float>(multiply(a, b)));
+}
+
+BOOST_AUTO_TEST_CASE(test_multiply_sfixed) {
+  SQ7x8 a = 100.0;
+  SQ7x8 b = 1.5;
+  BOOST_CHECK( 150.0 == static_cast<float>(multiply(a, b)));
+}
+
 
 BOOST_AUTO_TEST_SUITE_END()
